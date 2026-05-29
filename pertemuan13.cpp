2b48e8464ebb1d24101f5a07941deba992376899
khalaffarhan <khalaffarhan54@gmail.com>
2026-05-29 16:39:12 +0700

    commit 10

--- pertemuan13.cpp
+++ pertemuan13.cpp
@@ -87,4 +87,14 @@ public:
 
         cout << "\nElement in the queue are....\n";
 
+        // jika FRONT <= REAR, interasi dari FRONT hingga REAR
+        if (FRONT_Position <= REAR_Position)
+        {
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
         
